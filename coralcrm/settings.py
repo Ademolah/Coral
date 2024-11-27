39f04c8712132b7b2f1bbdf89ba1741c33641a4a
Charles <adeakinyemi129@gmail.com>
2024-11-27 17:07:12 +0100

    created user profile

--- coralcrm/settings.py
+++ coralcrm/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'userprofile'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'coralcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coralcrm',
+        'USER': 'root',
+        'PASSWORD': 'Demola321@',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
